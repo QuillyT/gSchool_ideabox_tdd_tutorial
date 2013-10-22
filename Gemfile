5702fdbf2898a7ca098d3fc49ba2fdb761172234
QuillyT <QuillyT@gmail.com>
2013-10-21 22:27:07 -0600

    Saving extra additions..

--- Gemfile
+++ Gemfile
@@ -5,4 +5,6 @@ gem 'sinatra', require: 'sinatra/base'
 group :test do
   gem 'minitest', require: false
   gem 'rack-test', require: false
+  gem 'capybara', require: false
+  gem 'minitest-capybara', require: false
 end
---
+++ test/acceptance/idea_management_test.rb
@@ -0,0 +1,31 @@
+require './test/test_helper'
+require 'bundler'
+Bundler.require
+require 'rack/test'
+require 'capybara'
+require 'capybara/dsl'
+
+require './lib/app'
+
+Capybara.app = IdeaboxApp
+
+Capybara.register_driver :rack_test do |app|
+  Capybara::RackTest::Driver.new(app, :headers => { 'User-Agent' => 'Capybara' })
+end
+
+class IdeaManagementTest < Minitest::Test
+  include Capybara::DSL
+
+  def teardown
+    IdeaStore.delete_all
+  end
+
+  def test_manage_ideas
+    visit '/'
+    fill_in 'title', :with => 'eat'
+    fill_in 'description', :with => 'chocolate chip cookies'
+    click_button 'Save'
+    assert page.has_content?("chocolate chip cookies"), "Idea is not on page"
+
+  end
+end
