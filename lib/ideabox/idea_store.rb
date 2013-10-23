f3e7b23f0a423eb0a114d02717040ddc6bf8f637
QuillyT <QuillyT@gmail.com>
2013-10-23 14:52:20 -0600

    Add YAML db.

--- lib/ideabox/idea_store.rb
+++ lib/ideabox/idea_store.rb
@@ -1,17 +1,36 @@
+require 'yaml/store'
+
 class IdeaStore
   class << self
     def save(idea)
-      idea.id ||= next_id
-      all[idea.id] = idea
+      idea.id ? update(idea) : create(idea)
+    end
+
+    def create(idea)
+      idea.id = next_id
+      database.transaction do
+        database['ideas'] << idea
+      end
+      idea.id
+    end
+
+    def update(idea)
+      database.transaction do
+        database['ideas'][idea.id] = idea
+      end
       idea.id
     end
 
     def all
-      @all ||= []
+      database.transaction do |db|
+        db['ideas'] || []
+      end
     end
 
     def find(id)
-      all[id]
+      database.transaction do
+        database['ideas'].find { |idea| idea.id == id }
+      end
     end
 
     def count
@@ -19,24 +38,42 @@ class IdeaStore
     end
 
     def next_id
-      #all.size
-      @counter ||= -1
-      @counter += 1
+      database.transaction do
+        @database['next_id'] ||= -1
+        @database['next_id'] += 1
+      end
     end
 
     def delete(id)
-      all.delete_at(id)
+      database.transaction do
+        database['ideas'].delete_if{ |idea| idea.id==id }
+      end
     end
 
     def delete_all
-      @all = []
-      @counter = -1
+      database.transaction do
+        database['ideas'] = []
+        database['next_id'] = -1
+      end
     end
 
     def find_by_title(text)
       all.find do |idea|
         idea.title == text
       end
+    end
+
+    def database
+      return @database if @database
+      @database ||= YAML::Store.new "ideabox_#{environment}"
+      @database.transaction do
+        @database['ideas'] ||= []
+      end
+      @database
+    end
+
+    def environment
+      ENV['RACK_ENV'] || 'development'
     end
   end
 end
